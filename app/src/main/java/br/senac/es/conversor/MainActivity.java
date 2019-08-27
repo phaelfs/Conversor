b2fa48f637dc05a3eafa7e1fe8820330bde592bf
Sala 302B <sala302b@ceines.com.br>
2019-08-26 21:58:09 -0300

    26-08

--- app/src/main/java/br/senac/es/conversor/MainActivity.java
+++ app/src/main/java/br/senac/es/conversor/MainActivity.java
@@ -3,12 +3,33 @@ package br.senac.es.conversor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int alturaEmCentimentros = 0;
+ /*
+* Protected - Somente classe atual e herdeiros podem usar
+* Void - Retorna Vazio
+* OnCreate - Nome do método
+* Override - Sobrescrito*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Metodo que liga a MaisActivity.JAVA no activity_mais.xml (TELA)
         setContentView(R.layout.activity_main);
+        /*
+        Classe - Declara os componentes
+        Instancia - Objeto Criado
+         */
+
+        //INSTACIA DE UM ELEMENTO TEXTO DO ANDROID
+        final TextView txtMetros = (TextView) findViewById(R.id.txtMetros);
+        //EXEMPLO CONVENCIONAL DE INSTANCIAR OBJETO NO JAVA
+        //final TextView txtMetros = new TextView();
+
+        final TextView txtPes = ()
     }
 }
