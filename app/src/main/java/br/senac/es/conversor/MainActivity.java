52a7a71bd3886ddb8bff74305bbc5b2d5f391128
Sala 302B <sala302b@ceines.com.br>
2019-08-29 21:53:10 -0300

    29-08

--- app/src/main/java/br/senac/es/conversor/MainActivity.java
+++ app/src/main/java/br/senac/es/conversor/MainActivity.java
@@ -9,12 +9,18 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
+
 //METODO E FUNCAO = MESMA COISA
 //DEFINICAO DA CLASSE        ---    HERDA ATRIBUTOS DE OUTRA CLASSE
 public class MainActivity extends AppCompatActivity {
 
     //DECLARACAO DE VARIAVEL    --   0 SIGNIFICA QUE O VALOR INICIAL É ZERO
     int alturaEmCentimentros = 0;
+
+    int altura1 = 0;
+    int altura2 = 1;
+    int altura3 = 2;
+
 
     //DECLARACAO DE VARIAVEL DO TIPO DOUBLE      -     O QUE ESTA ENTRE PARENTESES É ARGUMENTO / PARAMETRO
     private String formataValorComDoisDigitos(double valor) {
@@ -44,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
         Instancia - Objeto Criado
          */
 
+        final TextView txtMedia = (TextView) findViewById(R.id.txtMedia);
+        txtMedia.setText("MEDIA");
+
+
         //INSTACIA DE UM ELEMENTO TEXTO DO ANDROID
         final TextView txtMetros = (TextView) findViewById(R.id.txtMetros);
 
@@ -63,24 +73,24 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
-//SOBRESCREVE O METODO HERDADO
+            //SOBRESCREVE O METODO HERDADO
             @Override
             //DEFININDO A SEEKBAR PARA EXIBIR O VALOR NA TELA ENQUANTO SELECIONAMOS O TAMANHO
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
                 //DECLARANDO NOVO VALOR À VARIAVEL
                 alturaEmCentimentros = progress;
 
                 //FORMATA O VALOR "TEXTO" PARA EXIBIR DOIS DIGITOS
                 String texto = formataValorComDoisDigitos(progress / 100.0);
 
-
                 texto += " m. ";
-
+//
                 txtMetros.setText(texto);
             }
 
             @Override //DIFININDO TEXTO EXIBIDO ENQUANTO NÃO CLICAMOS EM CONVERTER
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(SeekBar seekBar) {//metodo
                 txtPes.setText("Toque em Converter");
             }
 
@@ -91,18 +101,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //INSTANCIA DO BOTÃO DE CONVERSÃO
+        //
         final Button button = (Button) findViewById(R.id.button);
+
+        //ADICIONANDO NOVO EVENTO
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override //SETANDO FUNCÃO DO BOTÃO AO CLICAR
             public void onClick(View v) {
+
+                //PEGA VARIAVEL "alturaEmPes'  E ATRIBUI 'alturaEmCentimentros' (Valor da SeekBar) Dividido por 30.48
                 double alturaEmPes = alturaEmCentimentros / 30.48;
 
+                //DEFINE VALOR À STRING TEXTO
                 String texto = formataValorComDoisDigitos(alturaEmPes);
+
+                //TEXTO CONCATENADO COM A STRING
                 texto += " pé(s)";
                 txtPes.setText(texto);
             }
         });
+
+        int media = calcularMedia(altura1, altura2, altura3);
+
+    }
+
+    private int calcularMedia(int altura1, int altura2, int altura3) {
+        int media = altura1 + altura2 + altura3 / 3;
+        return media;
     }
 
 }
